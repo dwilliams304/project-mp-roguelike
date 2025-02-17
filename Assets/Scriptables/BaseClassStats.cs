da61e13b36fe6b8bfbc67a27713ef0e600273a7e
Davis W <d.williams0434@gmail.com>
2025-02-16 21:04:55 -0500

    add entity stats, start work on stat scriptables

---
+++ Assets/Scriptables/BaseClassStats.cs
@@ -0,0 +1,38 @@
+using ContradictiveGames.Systems.Stats;
+using UnityEngine;
+
+
+public enum EntityFaction {
+    Player,
+    Undead,
+    Beast,
+    Critter,
+    Demon,
+    Humanoid
+}
+
+
+namespace ContradictiveGames.Scriptables
+{
+    [CreateAssetMenu(fileName = "Class Stats", menuName = "Contradictive Games/Entities/Stats")]
+    public class EntityStats : ScriptableObject
+    {
+        [Header("Health Related")]
+        public Stat MaxHealth;
+        [Tooltip("How long do we wait before we last took damage, and start regenerating health?")]
+            public float HealthRegenWait;
+        [Tooltip("How long we wait in between health ticks")]
+            public float HealthRegenSpeed;
+        
+        [Header("Combat Related")]
+        public Stat BaseDamage;
+        public Stat PrimaryAttackSpeed;
+        public Stat SecondaryAttackSpeed;
+        public Stat CriticalStrikeChance;
+        public Stat CriticalStrikeDamage;
+        public Stat DamageTaken;
+        public Stat HitChance;
+        
+        public Stat MoveSpeed;
+    }
+}
