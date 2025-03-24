7d763b570d1a052d7037eb7088279927da631d60
Davis W <d.williams0434@gmail.com>
2025-03-24 16:02:45 -0400

    wip: gamemanager

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 namespace ContradictiveGames.Managers
 {
+    [System.Flags]
+    public enum LogColor
+    {
+        black = 1,
+        white = 2,
+        green = 3,
+        cyan = 4,
+        magenta = 5,
+    }
+    
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
@@ -20,11 +30,18 @@ namespace ContradictiveGames.Managers
 
         private GameState currentState;
 
+
         
         [Header("Game Settings")]
         [Tooltip("How long we want the initial countdown to be")] [SerializeField] private float countdownTimer;
         [Tooltip("How long in seconds can the game go on")] [SerializeField] private float maxGameTime;
         private float currentGameTimer; //Current timer count
+
+
+
+        [Header("DEVELOPMENT SETTINGS")]
+        [SerializeField] private bool skipCountdownTimer;
+        [SerializeField] private LogColor gameStateLogColor;
 
         private void Awake()
         {
@@ -36,6 +53,41 @@ namespace ContradictiveGames.Managers
             }
             if(transform.parent) transform.parent = null;
             DontDestroyOnLoad(this);
+
+
+            //DEBUG ONLY, SWITCH TO ACTIVE IMMEDIATELY
+            if(skipCountdownTimer){
+                ChangeGameState(GameState.GameActive);
+                Debug.Log($"<color=green>We currently are in debug mode, and are skipping the countdown timer!</color>");
+            }
+            else{
+                ChangeGameState(GameState.PreparingLevel);
+            }
+        }
+
+
+        private void ChangeGameState(GameState state)
+        {
+            currentState = state;
+            switch(state){
+                case GameState.PreparingLevel:
+                    Debug.Log($"<color={gameStateLogColor}>GAME STATE: </color>Preparing level...");
+                    break;
+                case GameState.WaitingToStart:
+                    Debug.Log($"<color={gameStateLogColor}>GAME STATE: </color>Waiting to start...");
+                    break;
+                case GameState.GameCountdown:
+                    Debug.Log($"<color={gameStateLogColor}>GAME STATE: </color>Starting countdown...");
+                    break;
+                case GameState.GameActive:
+                    Debug.Log($"<color={gameStateLogColor}>GAME STATE: </color>Game is now active...");
+                    break;
+                case GameState.GameOver:
+                    Debug.Log($"<color={gameStateLogColor}>GAME STATE: </color>Game is over...");
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
